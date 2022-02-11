4e562cc000f2fcea0fdc278b2d598f9aad401a23
Hargaaya <hargaaya10@gmail.com>
2022-02-11 17:12:41 +0100

    Login component

---
+++ react-badui/src/components/Login.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="container mx-auto max-w-md rounded-sm bg-slate-200 p-3 shadow-md">
+      <form className="flex flex-col" id="login">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          data-lpignore="true"
+        />
+        <label>Username</label>
+
+        <input
+          type="password"
+          placeholder="Password"
+          id="username"
+          data-lpignore="true"
+        />
+        <label>●●●●●●●●</label>
+
+        <input type="reset" type="submit" value="Clear form" id="clearButton" />
+        <input type="button" value="Log in" id="loginButton" />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
